3a9eb2f4d663e43b7a0a84ad97f9bf752700091b
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-09-18 20:06:13 +0530

    🚀 day2

---
+++ 02variables/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var myint uint8 = 234
+
+	var num = "Shreyas"
+	num = "Shrey"
+
+	const myfloat64 float64 = 890.0993398
+
+	var name string = "Shreyas"
+
+	fmt.Printf(name + "\n")
+	fmt.Println(myint)
+	fmt.Println(num)
+	fmt.Println(myfloat64)
+}
