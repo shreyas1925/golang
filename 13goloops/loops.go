2ec345af546e441d5c46ff03f3d141112a0b2727
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-09-29 23:49:56 +0530

    ✅ day 13

--- 13goloops/loops.go
+++ 13goloops/loops.go
@@ -25,4 +25,27 @@ func main() {
 		fmt.Println(emp)
 	}
 
+	randomnumber := 0
+
+	for randomnumber < 10 {
+
+		if randomnumber == 2 {
+			goto statement
+		}
+
+		if randomnumber == 5 {
+			randomnumber++
+			continue
+		}
+
+		if randomnumber > 7 {
+			break
+		}
+		fmt.Println("The random number is %v", randomnumber)
+		randomnumber++
+	}
+
+statement:
+	fmt.Println("We have reached the milestone of randomnumber list ✌️")
+
 }
