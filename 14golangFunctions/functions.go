d76389e0ad7b91593b049a4b127d30da52767ca7
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-10-01 12:27:00 +0530

    ✅day 14

--- 14golangFunctions/functions.go
+++ 14golangFunctions/functions.go
@@ -1,10 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	fmt.Println("Let's go through the go functions 😉")
-	fmt.Println("style jsx")
+	// fmt.Println("Let's go through the go functions 😉")
 
 }
